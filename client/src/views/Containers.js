b9e17dbb2a55e997550b29ba925ab24f1db3fce5
Karel Šoupa <karelsoupa@gmail.com>
2017-10-22 20:35:15 +0200

    Windows support

---
+++ client/src/views/Containers.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Containers extends Component {
+
+  render() {
+    return (
+      <div className="animated fadeIn">
+        Docker Containers
+      </div>
+    )
+  }
+}
+
+export default Containers;
