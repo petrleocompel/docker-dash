3aecc7537020410e847ea653046f5d4c1ab17424
Emil Segeč <segec.emil@seznam.cz>
2017-11-23 20:51:05 +0100

    client update - fixed bug in dropdown menu when all dropdowns pop up at same time

--- client/src/InstanceCard.js
+++ client/src/InstanceCard.js
@@ -1,20 +1,19 @@
 import React, {Component} from "react"
 import PropTypes from 'prop-types'
 import {mapToCssModules} from 'reactstrap/lib/utils'
-import {responsr, getAll, instanceStop, instanceRestart, instanceStart} from './api'
-import {ButtonDropdown, ButtonGroup, Card, CardBlock, Col, DropdownItem, DropdownMenu, DropdownToggle,Row} from 'reactstrap'
-import Loader from './Loader'
+import {ButtonDropdown, ButtonGroup, Card, CardBlock, DropdownItem, DropdownMenu, DropdownToggle} from 'reactstrap'
+
 
 const propTypes = {
     header: PropTypes.string,
     mainText: PropTypes.string,
     smallText: PropTypes.string,
-    color: PropTypes.string,
-    value: PropTypes.string,
-    children: PropTypes.node,
-    className: PropTypes.string,
-    cssModule: PropTypes.object,
-    variant: PropTypes.string
+    statusColor: PropTypes.string,
+    start: PropTypes.func,
+    stop: PropTypes.func,
+    restart: PropTypes.func,
+    key: PropTypes.string,
+
 };
 
 const defaultProps = {
@@ -27,8 +26,6 @@ const defaultProps = {
 };
 
 class InstanceCard extends React.Component {
-
-    state = {images: null};
 
     constructor(props) {
         super(props);
@@ -45,77 +42,38 @@ class InstanceCard extends React.Component {
         });
     }
 
-    componentDidMount() {
-        responsr(getAll()).then((data) => {
-            this.setState({images: data.others});
-
-
-        })
-    }
-
-    handleStop = (id) => {
-        instanceStop(id);
-    };
-
-    handleStart = (id) => {
-        instanceStart(id);
-    };
-
-    handleRestart = (id) => {
-        instanceRestart(id);
-    };
-
-    handleList = (id) => {
-        alert('Listing item TODO')
-    };
-
 
     render() {
-        if (!this.state.images) {
-            return <Loader/>
 
-        }
+        const{header, mainText, smallText, start, stop, restart, statusColor, key} = this.props;
 
         return (
-            <Row> { this.state.images.map((item, index) => {
+            <Card className={"text-white bg-" + statusColor}>
+                <CardBlock className="card-body pb-0">
+                    <ButtonGroup className="float-right">
+                        <ButtonDropdown id={key} isOpen={this.state.card1}
+                                        toggle={() => {
+                                            this.setState({card1: !this.state.card1});
+                                        }}>
+                            <DropdownToggle caret className="p-0" color="transparent">
+                                <i className="icon-settings"></i>
+                            </DropdownToggle>
+                            <DropdownMenu className={this.state.card1 ? "show" : ""} right>
+                                <DropdownItem onClick={start}>Start</DropdownItem>
+                                <DropdownItem onClick={stop}>Stop</DropdownItem>
+                                <DropdownItem onClick={restart}>Restart</DropdownItem>
+                            </DropdownMenu>
+                        </ButtonDropdown>
+                    </ButtonGroup>
+                    <div className="h4 m-0">{header}</div>
+                    <div>{mainText}</div>
+                    <small className="text-muted">{smallText}</small>
+                </CardBlock>
+            </Card>
 
-
-                let statusColor;
-
-                if (item.status.indexOf("Up") !== -1) {
-                    statusColor = 'success';
-                }
-                else if(item.status.indexOf("Exited") !== -1){
-                    statusColor = 'danger';
-                }
-                return <Col key={index} xs="6" sm="4">
-                    <Card className={"text-white bg-" + statusColor}>
-                        <CardBlock className="card-body pb-0">
-                            <ButtonGroup className="float-right">
-                                <ButtonDropdown id={index} isOpen={this.state.card1}
-                                                toggle={() => {
-                                                    this.setState({card1: !this.state.card1});
-                                                }}>
-                                    <DropdownToggle  caret className="p-0" color="transparent">
-                                        <i className="icon-settings"></i>
-                                    </DropdownToggle>
-                                    <DropdownMenu className={this.state.card1 ? "show" : ""} right>
-                                        <DropdownItem onClick={() => (this.handleStart(item.id))}>Start</DropdownItem>
-                                        <DropdownItem onClick={() => (this.handleStop(item.id))}>Stop</DropdownItem>
-                                        <DropdownItem onClick={() => (this.handleRestart(item.id))}>Restart</DropdownItem>
-                                    </DropdownMenu>
-                                </ButtonDropdown>
-                            </ButtonGroup>
-                            <div className="h4 m-0">{item.name}</div>
-                            <div>{item.image}</div>
-                            <small className="text-muted">{item.status}</small>
-                        </CardBlock>
-                    </Card>
-                </Col>
-
-            })}
-            </Row>)
+        )
     }
+
 }
 
 InstanceCard.propTypes = propTypes;
--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -1,13 +1,73 @@
 import React, {Component} from 'react'
 import InstanceCard from "../InstanceCard";
+import Loader from '../Loader'
+import {responsr, getAll, instanceStop, instanceRestart, instanceStart} from '../api'
+import {Col, Row} from "reactstrap";
 
 
 class Dashboard extends React.Component {
 
+    state = {images: null};
+
+    componentDidMount() {
+        responsr(getAll()).then((data) => {
+            this.setState({images: data.others});
+
+
+        })
+    }
+
+    handleStop = (id) => {
+        instanceStop(id);
+    };
+
+    handleStart = (id) => {
+        instanceStart(id);
+    };
+
+    handleRestart = (id) => {
+        instanceRestart(id);
+    };
+
+    handleList = (id) => {
+        alert('Listing item TODO')
+    };
+
     render() {
 
 
-        return <InstanceCard/>
+        if (!this.state.images) {
+            return <Loader/>
+
+        }
+
+        return (
+            <Row> {this.state.images.map((item, index) => {
+
+
+                let status;
+
+                if (item.status.indexOf("Up") !== -1) {
+                    status = 'success';
+                }
+                else if (item.status.indexOf("Exited") !== -1) {
+                    status = 'danger';
+                }
+                return <Col xs="6" sm="4">
+                    <InstanceCard
+                        key={index}
+                        statusColor={status}
+                        header={item.name}
+                        mainText={item.image}
+                        smallText={item.status}
+                        start={() => this.props.dispatch(this.handleStart(item.id))}
+                        stop={() => this.props.dispatch(this.handleStop(item.id))}
+                        restart={() => this.props.dispatch(this.handleRestart(item.id))}
+                    /></Col>
+
+
+            })}
+            </Row>)
 
     }
 }
